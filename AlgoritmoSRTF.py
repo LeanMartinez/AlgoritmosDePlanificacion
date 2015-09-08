a45488c24e7a2873e41d417422bd1ac238ca5d02
leomaster01 <leandro.martinez01@gmail.com>
2015-09-08 20:04:20 -0300

    realizo métodos para obtener la primera ejecucion y restar cpu

---
+++ AlgoritmoSRTF.py
@@ -0,0 +1,22 @@
+lista = [['p1',3,2],['p2',4,0],['p3',3,1]]
+
+
+def obtenerPrimeraEjecucion(lista):
+	i = 1
+	for x in lista:
+		if i == 1:
+			llegada = x[2]
+			i = i + 1
+		else:
+			if x[2] < llegada:
+				llegada = x[2]
+				return x 
+
+def restarCPU(proceso):
+	proceso[1] = proceso[1] -1
+	return proceso
+
+proceso =  obtenerPrimeraEjecucion(lista)
+print restarCPU(proceso)
+
+

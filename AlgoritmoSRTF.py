effdea085e6a6098b0e9e5dbdbfc45a30db6524d
dhernanromero <dhernan.romero@gmail.com>
2015-09-19 19:40:12 -0300

    Realizo cambios

--- AlgoritmoSRTF.py
+++ AlgoritmoSRTF.py
@@ -1,6 +1,7 @@
-lista = [['p1',3,2],['p2',4,0],['p3',3,1]]
+lista = [['p1',3,1],['p2',5,0],['p3',2,3]]
+#lista = [['p1',3,0],['p2',1,2],['p3',2,3]]
+instanciasDeTiempo = 0
 
-#esto es una prueba
 def obtenerPrimeraEjecucion(lista):
 	i = 1
 	for x in lista:
@@ -16,7 +17,47 @@ def restarCPU(proceso):
 	proceso[1] = proceso[1] -1
 	return proceso
 
+def comparar(proceso, lista, instanciasDeTiempo):
+	if proceso[1] == 0:
+		for x in lista:
+			if x[0] == proceso[0]:
+				ubicacion = lista.index(x)
+				lista.pop(ubicacion)
+				if lista==[]:
+					print "Fin"
+				else:
+					return lista[ubicacion-1]
+	else:			
+		siguienteProceso = None
+		for x in lista:
+			if len(x) > 3:
+				if proceso[1] > x[1]:
+					siguienteProceso = x
+				else:
+					if siguienteProceso == None:
+						siguienteProceso = proceso
+			else:
+				if proceso[1] > x[1] and x[2] == instanciasDeTiempo:
+					siguienteProceso = x
+				else:
+					if siguienteProceso == None:
+						siguienteProceso = proceso
+		
+			
+		return siguienteProceso
+
+
 proceso =  obtenerPrimeraEjecucion(lista)
-print restarCPU(proceso)
+for x in range(10):
+	print proceso, instanciasDeTiempo
+	if proceso == lista[lista.index(proceso)] and len(proceso) == 3:
+		proceso.append(1)
+		restarCPU(proceso)
+
+	else:
+		restarCPU(proceso)
+	print proceso, instanciasDeTiempo
+	instanciasDeTiempo = instanciasDeTiempo + 1
+	proceso = comparar(proceso,lista, instanciasDeTiempo)
 
 
